4ff6f66b01e985671f4eca70987cb6521b999c12
User <User@BCLASS-01>
2021-01-21 18:43:00 +0900

    구구단 가로 배열

---
+++ D07/D07/d7.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+	printf("\n=======±¸±¸´Ü=======\n");
+	for (int x = 1; x <= 9; x++) 
+	{
+		
+		for (int y = 1; y <= 9; y++) {
+
+			printf(" %d x %d=%d\t ", x, y, x * y);
+		}
+
+		printf("\n");
+	}
+	return 0;
+}
